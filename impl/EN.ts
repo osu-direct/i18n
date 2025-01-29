396e7088bb23969727803ee8d86302a623e97691
HorizonCode <horizoncode88@gmail.com>
2025-01-29 16:24:29 +0100

    initial commit

---
+++ impl/EN.ts
@@ -0,0 +1,11 @@
+import type { Language } from '../language';
+
+const language: Language = {
+	name: 'English',
+	code: 'EN',
+	aliases: ['US', 'GB'],
+	translator: 'HorizonCode',
+	phrases: {}
+};
+
+export default language;
---
+++ language.ts
@@ -0,0 +1,34 @@
+import US from "./impl/EN";
+import DE from "./impl/DE";
+
+export type Language = {
+  name: string;
+  code: string;
+  aliases?: string[];
+  translator: string;
+  phrases: { [key: string]: string };
+};
+
+export const languages: Language[] = [US, DE];
+
+export const getLanguage = (code: string) =>
+  languages.find((lang) => lang.code === (code?.toUpperCase() ?? "EN")) ||
+  languages.find((lang) => lang.aliases?.includes(code?.toUpperCase() ?? "EN"));
+
+export const __ = (
+  key: string,
+  lang: string,
+  placeholders?: { [key: string]: string | number }
+) => {
+  const language = getLanguage(lang);
+  let translation = language?.phrases[key] ?? key;
+  if (placeholders) {
+    for (const [placeholder, value] of Object.entries(placeholders)) {
+      translation = translation.replace(
+        new RegExp(`{{${placeholder}}}`, "g"),
+        value.toString()
+      );
+    }
+  }
+  return translation;
+};
